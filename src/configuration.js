eb6a64bc7842f567a05f383df170dcef2d697c9d
frasfras <65541080+frasfras@users.noreply.github.com>
2020-10-28 03:44:31 +0800

    update configjs
    config js

--- src/configuration.js
+++ src/configuration.js
@@ -2,7 +2,7 @@ const config = {
   header: "Header",
   headerBackground: "#ff8ba7",
   bodyBackground: "#ffc6c7",
-  title: "Custom1",
+  title: "Custom1 -test",
   subTitle: "Finished Furniture ",
   airtable: "https://airtable.com/embed/shrg1geAbjcffqePs?backgroundColor=purple&viewControls=on",
   googleAnalytics: "UA-XXXXXXXX-X"
