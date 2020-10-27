d3fabb0aae4d26b2fe637570630dfd658aac73ec
frasfras <65541080+frasfras@users.noreply.github.com>
2020-10-28 03:28:29 +0800

    Update configuration.js

--- src/configuration.js
+++ src/configuration.js
@@ -4,7 +4,7 @@ const config = {
   bodyBackground: "#ffc6c7",
   title: "Custom",
   subTitle: "Finished Furniture ",
-  airtable: "https://airtable.com/shrMp9y0Hwyp5AtCY?backgroundColor=purple&viewControls=on",
+  airtable: "https://airtable.com/shrg1geAbjcffqePs?backgroundColor=purple&viewControls=on",
   googleAnalytics: "UA-XXXXXXXX-X"
 }
 
