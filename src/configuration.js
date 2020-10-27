a57dbdfad827de9bc206ef3619f632d7cc762b1d
frasfras <65541080+frasfras@users.noreply.github.com>
2020-10-28 03:13:40 +0800

    Update configuration.js

--- src/configuration.js
+++ src/configuration.js
@@ -2,9 +2,9 @@ const config = {
   header: "Header",
   headerBackground: "#ff8ba7",
   bodyBackground: "#ffc6c7",
-  title: "Title",
-  subTitle: "Sub Title",
-  airtable: "https://airtable.com/embed/shrdi24ocwoEeXx6d?backgroundColor=red&viewControls=on",
+  title: "Custom",
+  subTitle: "Finished Furniture ",
+  airtable: "https://airtable.com/embed/shrdi24ocwoEeXx6d?backgroundColor=purple&viewControls=on",
   googleAnalytics: "UA-XXXXXXXX-X"
 }
 
