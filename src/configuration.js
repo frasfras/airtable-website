eaf48545dd960c472bbc7f46256b80cff69fe275
frasfras <65541080+frasfras@users.noreply.github.com>
2020-10-28 09:11:17 +0800

    Update configuration.js

--- src/configuration.js
+++ src/configuration.js
@@ -4,7 +4,7 @@ const config = {
   bodyBackground: "#ffc6c7",
   title: "Custom1 -test",
   subTitle: "Finished Furniture ",
-  airtable: https://community.airtable.com/t/make-your-airtable-go-live-on-a-custom-domain-for-free/32422
+  airtable: "https://community.airtable.com/t/make-your-airtable-go-live-on-a-custom-domain-for-free/32422"
   ,
   googleAnalytics: "UA-XXXXXXXX-X"
 }
